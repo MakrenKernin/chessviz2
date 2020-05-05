5c05c8b2cfbd105391a7bd54b12d94f8638117c7
icymane <egor.andrianov34@gmail.com>
2020-05-05 21:06:31 +0700

    Add

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -3,13 +3,13 @@
 
 void print_board(char board[][8])
 {
-	printf("A B C D E F G H\n");
-	for (int i = 0; i < 8; i++) {
-		printf("%d |", i + 1);
-		for (int j = 0; j < 8; j++) {
-			printf("%c ", board[i][j]);
-		}
-		printf("\n");
-	}
-	printf("A B C D E F G H\n");
+    printf("   A B C D E F G H\n   _______________\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%d |", i + 1);
+        for (int j = 0; j < 8; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("   _______________\n   A B C D E F G H\n");
 }
